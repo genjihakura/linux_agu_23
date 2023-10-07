49ebfc5450510d1f72ea1c6ee686cb97e467d3cd
genjihakura <you@example.com>
2023-10-07 19:28:30 +0700

    final assignment, first commit

---
+++ final/Final_Assignment/client.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/socket.h>     //  Chứa cấu trúc cần thiết cho socket.
+#include <netinet/in.h>     //  Thư viện chứa các hằng số, cấu trúc khi sử dụng địa chỉ trên internet
+#include <arpa/inet.h>
+#include <unistd.h>
+
+
+
+#define BUFF_SIZE 256
+#define handle_error(msg) \
+    do { perror(msg); exit(EXIT_FAILURE); } while (0)
+
+typedef struct _log_event {
+    int  SquenceNumber;
+    int  NodeID;
+    char Msg[255];
+    unsigned long int Timestamp;
+} log_event;
+void sendDatatoServer(int server_fd,float temperature );
+
+int main(int argc, char *argv[])
+{
+    int portno;
+    int server_fd;
+    struct sockaddr_in serv_addr;
+    float temperature;
+    memset(&serv_addr, '0',sizeof(serv_addr));
+    /* Đọc portnumber từ command line */
+    if (argc < 4) {
+        printf("command : ./client <server address> <port number>\n");
+        exit(1);
+    }
+    portno = atoi(argv[2]);
+    temperature = atof(argv[3]);
+    printf("line : %d portno :%d \n",__LINE__ ,portno);
+    /* Khởi tạo địa chỉ server */
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port   = htons(portno);
+    if (inet_pton(AF_INET, argv[1], &serv_addr.sin_addr) == -1){
+        handle_error("inet_pton()");
+    }
+    /* Tạo socket */
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == -1){
+        handle_error("socket()");
+    }
+    /* Kết nối tới server*/
+    if (connect(server_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1){
+
+        handle_error("connect()");
+    }
+    printf("line : %d connect() success !!!\n",__LINE__ );
+    sendDatatoServer(server_fd,temperature);
+    sleep(1);
+    sendDatatoServer(server_fd,temperature+1.f);
+    sleep(1);
+    sendDatatoServer(server_fd,temperature+0.5f); 
+    sleep(1);
+    printf("line : %d close() server_fd !!!\n",__LINE__ );      
+    close(server_fd); /*close*/
+    return 0;
+}
+
+
+/* Chức năng chat */
+void sendDatatoServer(int server_fd, float temperature)
+{
+    int numb_write, numb_read;
+    char Msg[256];
+    log_event sendbuff;
+    memset(Msg, '0', sizeof(Msg));
+    snprintf(Msg,sizeof(Msg),"%f",temperature);
+    printf("line : %d sendDatatoServer() !!!\n",__LINE__ );
+    numb_write = send(server_fd, &Msg, sizeof(Msg), 0);
+    if (numb_write == -1){
+        handle_error("send()");
+    }
+
+}
---
+++ final/Final_Assignment/server.c
@@ -0,0 +1,224 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <errno.h>
+#include <sys/socket.h>     //  Chứa cấu trúc cần thiết cho socket.
+#include <netinet/in.h>     //  Thư viện chứa các hằng số, cấu trúc khi sử dụng địa chỉ trên internet
+#include <arpa/inet.h>
+
+#define LISTEN_BACKLOG 50
+#define BUFF_SIZE 256
+#define handle_error(msg) do { perror(msg); exit(EXIT_FAILURE); } while (0)
+
+pthread_mutex_t lock_connect = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t lock_data = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t lock_manager = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond_connect = PTHREAD_COND_INITIALIZER;
+pthread_cond_t cond_data = PTHREAD_COND_INITIALIZER;
+
+int counter = 2; // shared variable/shared resources/global variable
+int port_no;
+int len, opt;
+int server_fd, new_socket_fd; 
+struct sockaddr_in serv_addr, client_addr;
+
+typedef struct _log_event {
+    int  SquenceNumber;
+    int  NodeID;
+    float Temperature;
+    unsigned long int Timestamp;
+} log_event;
+
+typedef struct _thread_args_t{
+        int IsChild;
+}thread_args_t;
+
+//share memory
+log_event dataSensor;
+/* Chức năng chat */
+void ServerReadDataAndWriteDataToShareStruct(int new_socket_fd)
+{
+    char recvbuff[BUFF_SIZE];
+    int numb_read, numb_write;
+    memset(&recvbuff, '0', sizeof(log_event));
+    numb_read = recv(new_socket_fd, &recvbuff, BUFF_SIZE, 0);
+    if(numb_read == -1){
+        handle_error("recv() form socket");
+    }
+    dataSensor.Temperature = atof(recvbuff);
+    printf("line : %d temperature : %f \n",__LINE__, dataSensor.Temperature );
+
+}
+
+
+//Server TCP
+//Listen on a TCP socket for incoming connect request from new sensor nodes
+static void *Connect_handle(void *args)
+{
+    thread_args_t *thr = (thread_args_t *)args;
+    pthread_mutex_lock(&lock_connect);
+    if(thr->IsChild == 0){
+    // log process thread
+
+    } else {
+    // Main process thread
+
+        memset(&serv_addr, 0, sizeof(struct sockaddr_in));
+        memset(&client_addr, 0, sizeof(struct sockaddr_in));
+
+        /* Tạo socket */
+        server_fd = socket(AF_INET, SOCK_STREAM, 0);
+        if (server_fd == -1){
+            handle_error("socket()");
+        }
+        // fprintf(stderr, "ERROR on socket() : %s\n", strerror(errno));
+
+        /* Ngăn lỗi : “address already in use” */
+        if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))){
+            handle_error("setsockopt()");
+        }
+
+        /* Khởi tạo địa chỉ cho server */
+        serv_addr.sin_family = AF_INET;
+        serv_addr.sin_port = htons(port_no);
+        serv_addr.sin_addr.s_addr =  INADDR_ANY; //inet_addr("192.168.5.128"); //INADDR_ANY
+
+        /* Gắn socket với địa chỉ server */
+        if (bind(server_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1){
+            handle_error("bind()");
+        }
+
+        /* Nghe tối đa 5 kết nối trong hàng đợi */
+        if (listen(server_fd, LISTEN_BACKLOG) == -1){
+            handle_error("listen()");
+        }
+
+        /* Dùng để lấy thông tin client */
+        len = sizeof(client_addr);
+        
+        while (1) {
+            printf("Server is listening at port : %d \n....\n",port_no);
+            new_socket_fd = accept(server_fd, (struct sockaddr*)&client_addr, (socklen_t *)&len);
+            if (new_socket_fd == -1)
+            {
+                handle_error("accept() no connect");
+            }
+            system("clear");
+            char temp[BUFF_SIZE];
+            inet_ntop(client_addr.sin_family, (struct sockaddr*)&client_addr, temp, sizeof(temp));
+            printf("Server : got connection \n");
+            ServerReadDataAndWriteDataToShareStruct(new_socket_fd);
+            // close(server_fd);
+        }
+    }
+    close(server_fd);
+    pthread_cond_signal(&cond_connect);
+    pthread_mutex_unlock(&lock_connect);
+    pthread_exit(NULL); // exit
+}
+//
+
+
+
+static void *Data_handle(void *args)
+{  
+    thread_args_t *thr = (thread_args_t *)args;
+    float sum = 0;
+    float avrg = 0;
+    pthread_mutex_lock(&lock_connect);
+    if(thr->IsChild == 0){
+        // log process thread
+    }else{
+        //Main process thread
+        //read data from share data, calculate a running average on the temperature  use the result to decide  on 'too/hot/cold'
+        //this function only read data and calculate 
+        sum += dataSensor.Temperature;
+    }
+    pthread_mutex_unlock(&lock_connect);
+    pthread_exit(NULL); // exit
+
+}
+//read sensor measurements and insert them to SQL
+static void *Storage_manager_handle(void *args)
+{  
+
+    thread_args_t *thr = (thread_args_t *)args;
+    pthread_mutex_lock(&lock_connect);
+    if(thr->IsChild == 0){
+        // log process thread
+    }else{
+        //Main process thread
+    }
+    pthread_mutex_unlock(&lock_connect);
+    pthread_exit(NULL); // exit
+}
+
+
+int main(int argc, char *argv[]){
+    int ret;
+    thread_args_t thr;
+    pthread_t thread_id1, thread_id2, thread_id3;
+
+    pid_t log_process = fork();
+    if( log_process == 0){
+        thr.IsChild = log_process;
+        // log_process is child process
+        if (ret = pthread_create(&thread_id1, NULL, &Connect_handle, &thr)) {
+            printf("pthread_create() error number=%d\n", ret);
+            return -1;
+        }
+
+        if (ret = pthread_create(&thread_id2, NULL, &Data_handle, NULL)) {
+            printf("pthread_create() error number=%d\n", ret);
+            return -1;
+        }
+
+        if (ret = pthread_create(&thread_id3, NULL, &Storage_manager_handle, NULL)) {
+            printf("pthread_create() error number=%d\n", ret);
+            return -1;
+        }
+
+        pthread_join(thread_id1,NULL);
+        pthread_cond_wait(&cond_connect, &lock_connect);
+        pthread_join(thread_id2,NULL);
+        pthread_cond_wait(&cond_connect, &lock_connect);
+        pthread_join(thread_id3,NULL);
+    }else{
+    //main process TBD
+        thr.IsChild = log_process;
+
+        /* Đọc portnumber trên command line */
+        if (argc < 2) {
+            printf("No port provided\ncommand: ./server <port number>\n");
+            exit(EXIT_FAILURE);
+        } else{
+                port_no = atoi(argv[1]);
+        }
+
+        if (ret = pthread_create(&thread_id1, NULL, &Connect_handle, &thr)) {
+            printf("pthread_create() error number=%d\n", ret);
+            return -1;
+        }
+
+        if (ret = pthread_create(&thread_id2, NULL, &Data_handle, NULL)) {
+            printf("pthread_create() error number=%d\n", ret);
+            return -1;
+        }
+
+        if (ret = pthread_create(&thread_id3, NULL, &Storage_manager_handle, NULL)) {
+            printf("pthread_create() error number=%d\n", ret);
+            return -1;
+        }
+        pthread_join(thread_id1,NULL);
+        pthread_cond_wait(&cond_connect, &lock_connect);
+        pthread_join(thread_id2,NULL);
+        pthread_cond_wait(&cond_connect, &lock_connect);
+        pthread_join(thread_id3,NULL);
+    }
+    return 0;
+}
